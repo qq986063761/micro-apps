7b7b6d9ced47682c9923fe2323e39d1c02eb31ca
WanPeng <986063761@qq.com>
2024-10-21 13:46:07 +0800

    更新

---
+++ wujie-main-demo/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <el-menu
+    class="el-menu-vertical-demo"
+    active-text-color="#ffd04b"
+    background-color="#545c64"
+    default-active="2"
+    text-color="#fff"
+  >
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <span></span>
+    </el-menu-item>
+  </el-menu>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
