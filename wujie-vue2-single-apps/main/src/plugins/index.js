ad5004ae96a75cdfdb734201117be46eec35385c
WanPeng <986063761@qq.com>
2025-11-02 14:08:52 +0800

    更新

--- wujie-vue2-single-apps/main/src/plugins/index.js
+++ wujie-vue2-single-apps/main/src/plugins/index.js
@@ -1,11 +1,13 @@
 import router from '@/router'
 import store from '@/store'
+import { getTheme } from '@/assets/theme'
 
 // 提供给子应用
 window.$mApp = {
   vm: null,
   store,
   router,
+  theme: getTheme(), // 主题变量，供子应用使用
   components: {}, // 提供给子应用的内联组件
   child1: {},
   child2: {},
