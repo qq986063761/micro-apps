a1901d9318f89240244dde753f56f29b03539d50
wanpeng <986063761@qq.com>
2026-01-19 19:46:50 +0800

    更新

--- wujie-vue3-apps/child2/src/main.js
+++ wujie-vue3-apps/child2/src/main.js
@@ -15,16 +15,3 @@ app.use(plugin)
 
 // 挂载应用
 const vm = app.mount('#app')
-
-// 如果是在微前端环境下，通知主应用初始化完成
-if (window.$wujie) {
-  const { bus } = window.$wujie
-  const { appName } = window.$wujie.props || {}
-  
-  if (appName && bus) {
-    bus.$emit('app:init', {
-      appName,
-      data: { timestamp: Date.now() }
-    })
-  }
-}
