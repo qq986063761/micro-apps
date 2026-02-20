2f351f8264487a728a4ef0ca401de5bc09391d36
wanpeng <986063761@qq.com>
2026-02-20 12:17:26 +0800

    更新

--- wujie-vue2-single-apps/child2/src/plugins/index.js
+++ wujie-vue2-single-apps/child2/src/plugins/index.js
@@ -1,17 +1,31 @@
 import router from '@/router'
 import store from '@/store'
 
+function shallowEqual(a, b) {
+  const A = a && typeof a === 'object' ? a : {}
+  const B = b && typeof b === 'object' ? b : {}
+  const keysA = Object.keys(A)
+  const keysB = Object.keys(B)
+  if (keysA.length !== keysB.length) return false
+  return keysA.every(k => A[k] === B[k])
+}
+
 // 提供给父应用
 window.$app = {
   vm: null,
   store,
   router,
   async to({ name = '', params, query, method = 'replace' }) {
-    if (router.currentRoute.name === name) return
+    console.log('child2 to', name, params, query, method)
+    const cur = router.currentRoute
+    const sameName = cur.name === name
+    const sameParams = shallowEqual(cur.params, params)
+    const sameQuery = shallowEqual(cur.query, query)
+    if (sameName && sameParams && sameQuery) return
     router[method]({
       name,
-      params,
-      query
+      params: params ?? {},
+      query: query ?? {}
     })
   },
   // 接收其他模块的数据监听事件
